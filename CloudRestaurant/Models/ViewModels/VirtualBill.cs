830d89df70c7e2ca3f83e0824058ffdbe3d94676
MohammedMazher1 <Mohammed711300411@outlook.sa>
2023-02-20 23:24:54 +0300

    Change logo and Bill

---
+++ CloudRestaurant/Models/ViewModels/VirtualBill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudRestaurant.Models.ViewModels
+{
+    public class VirtualBill
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int ItemQuantity { get; set; }
+    }
+}
