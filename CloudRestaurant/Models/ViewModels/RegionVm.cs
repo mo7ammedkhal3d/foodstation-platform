adc266610d833e891619b84d08d6b0fe8a1c76b1
MohammedMazher1 <Mohammed711300411@outlook.sa>
2023-04-04 00:12:20 +0300

    Region Work

---
+++ CloudRestaurant/Models/ViewModels/RegionVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudRestaurant.Models.ViewModels
+{
+    public class RegionVm
+    {
+        public string Name { get; set; }
+
+        public int CountryId { get; set; }
+    }
+}
